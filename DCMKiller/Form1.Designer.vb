2e3c5ca95f47428b7428af78e70d0890f43f9865
Liu Yixuan <wbsdty331@jnxy.edu.cn>
2016-04-23 17:50:23 +0800

    Upload Files.

---
+++ DCMKiller/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(133, 92)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(107, 12)
+        Me.LinkLabel1.TabIndex = 0
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "View it on Github"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 85)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 26)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "关于(&A)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(62, 25)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(119, 41)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "检测"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(252, 117)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "DCMKiller"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ DCMKiller/Form1.vb
@@ -0,0 +1,86 @@
+﻿
+
+Public Class Form1
+
+
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("http://www.baidu.com")
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MsgBox("DCMKiller" & vbCrLf & "本工具快速检测你的电脑是否感染DCM（黑暗幽灵）木马，并尝试清除。" & vbCrLf & vbCrLf & "请注意：本工具不能代替杀毒软件使用，如果怀疑中了此木马，请及时用ARK工具进行清除", MsgBoxStyle.Question, "About")
+
+    End Sub
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        On Error Resume Next
+        '通过环境变量来取Windows临时目录
+        Dim temppath, DCMPath
+
+        temppath = Environ("WINDIR") & "\Temp"
+        DCMPath = Environ("WINDIR") & "\Temp\{E53B9A13-F4C6-4d78-9755-65C029E88F02}"
+        '判断是否存在DCM的{E53B9A13-F4C6-4d78-9755-65C029E88F02}文件夹
+        If My.Computer.FileSystem.DirectoryExists(DCMPath) Then
+            If (MsgBox("检测到DCM工作目录，您的系统很有可能已感染DCM木马，是否尝试清除？", MsgBoxStyle.YesNo, "提示")) = 6 Then
+                '先删除TEMP里面的目录
+                My.Computer.FileSystem.DeleteDirectory(DCMPath, FileIO.DeleteDirectoryOption.DeleteAllContents)
+                '使用CMD命令修复LSP
+                Debug.WriteLine(CMD("netsh winsock reset"))
+                '然后开始判断系统版本（XP或以上）
+                Dim commonfiles = System.Environment.GetFolderPath(System.Environment.SpecialFolder.CommonProgramFiles)
+                Dim ver = System.Environment.OSVersion.Version.Major
+                If ver = 5 Then
+                    My.Computer.FileSystem.DeleteFile(Environ("WINDIR") & "\ntshrui.dll")
+                Else
+                    My.Computer.FileSystem.DeleteFile(Environ("WINDIR") & "\msls32.dll")
+                    My.Computer.FileSystem.DeleteFile(Environ("WINDIR") & "\AduioSes.dll")
+                End If
+                MsgBox("看起来应该清除完毕了。" & vbCrLf & "接下来，请使用ARK工具解除被挂的钩子和删除文件！", MsgBoxStyle.Question, "About")
+            Else
+                Exit Sub
+            End If
+        Else
+            MsgBox("未找到DCM工作目录。", MsgBoxStyle.Question, "提示")
+        End If
+
+    End Sub
+
+    Function CMD(ByVal Data As String) As String
+        Try
+            Dim p As New Process()
+            p.StartInfo.FileName = "cmd.exe"
+            p.StartInfo.UseShellExecute = False
+            p.StartInfo.RedirectStandardInput = True
+            p.StartInfo.RedirectStandardOutput = True
+            p.StartInfo.RedirectStandardError = True
+            p.StartInfo.CreateNoWindow = False
+            p.Start()
+            Application.DoEvents()
+            p.StandardInput.WriteLine(Data)
+            p.StandardInput.WriteLine("Exit")
+            Dim strRst As String = p.StandardOutput.ReadToEnd()
+            p.Close()
+            Return strRst
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+End Class
+
+
+
+
+
+
+
+
+
+
